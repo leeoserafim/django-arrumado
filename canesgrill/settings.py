3f66858137d431a5b94d0448cb5ccbd134f187fc
leeoserafim <132719804+leeoserafim@users.noreply.github.com>
2023-07-07 21:20:27 -0300

    123123

--- canesgrill/settings.py
+++ canesgrill/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleWare'
 ]
 
 ROOT_URLCONF = 'canesgrill.urls'
@@ -145,12 +146,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'templates/static')]
 STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
-STATIC_URL = 'staticfiles/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'templates/static',
-]
+STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # media
 MEDIA_ROOT = os.path.join(BASE_DIR , 'media' )
